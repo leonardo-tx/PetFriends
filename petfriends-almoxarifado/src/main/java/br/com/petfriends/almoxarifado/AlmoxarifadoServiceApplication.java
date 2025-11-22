5221a64d776b4c511e7bc783eeea709256b3327b
Leonardo Teixeira <littledoge02@gmail.com>
2025-11-22 13:14:05 -0300

    Adiciona projeto inicial

---
+++ petfriends-almoxarifado/src/main/java/br/com/petfriends/almoxarifado/AlmoxarifadoServiceApplication.java
@@ -0,0 +1,11 @@
+package br.com.petfriends.almoxarifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmoxarifadoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlmoxarifadoServiceApplication.class, args);
+    }
+}
---
+++ petfriends-transporte/src/main/java/br/com/petfriends/transporte/TransporteServiceApplication.java
@@ -0,0 +1,11 @@
+package br.com.petfriends.transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransporteServiceApplication.class, args);
+    }
+}
